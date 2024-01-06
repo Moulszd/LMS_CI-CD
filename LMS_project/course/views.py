7372b51b6eb825fa49229d83e8552ccda6344d33
Ashekfeh <Abd.shekfeh@Gmail.com>
2024-01-07 00:52:39 +0300

    added course model serializer

--- LMS_project/course/views.py
+++ LMS_project/course/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .models import Course, Category
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .serializers import CategroySerializer
+
+
+# class CourseViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = Course.objects.all().order_by('-date_joined')
+#     serializer_class = CourseSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = Category
+    permission_classes = [permissions.IsAuthenticated]
